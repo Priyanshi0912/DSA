b950bc993ce20a9029d5ce10a46a4ec0bf793fa1
Priyanshi <101731203+Priyanshi0912@users.noreply.github.com>
2024-02-18 00:05:40 +0530

    Update Furthest Building You Can Reach.py

--- Furthest Building You Can Reach.py
+++ Furthest Building You Can Reach.py
@@ -1,16 +1,16 @@
 class Solution:
     def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:
-        n=len(heights)
-        min_heap=[]
-        for i in range(n-1):
-            diff=heights[i+1]-heights[i]
-            if diff>0:
-                min_heap.append(diff)
-                print(min_heap)
-            if len(min_heap)>ladders:
-                bricks-=min(min_heap)
-                print("bricks remaining:",bricks)
-                min_heap.remove(min_heap.index(min(min_heap)))
-            if bricks<0:
-                return i
+        n = len(heights)
+        heap=[]
+        for i in range(1, n):  
+            jump=heights[i]-heights[i-1]
+            if jump>0:
+                heapq.heappush(heap, jump)
+            if len(heap)>ladders:
+                bricks-= heapq.heappop(heap)
+                if bricks < 0: return i-1
         return n-1
+                
+
+
+        
