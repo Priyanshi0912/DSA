27aed3d8b9179d069cb6f562eb3f65c51f85dc88
Priyanshi <101731203+Priyanshi0912@users.noreply.github.com>
2024-04-08 23:56:38 +0530

    Update 1249. Minimum Remove to Make Valid Parentheses.py

--- 1249. Minimum Remove to Make Valid Parentheses.py
+++ 1249. Minimum Remove to Make Valid Parentheses.py
@@ -2,6 +2,7 @@ class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
         stack = []
         s = list(s)
+        
         for i in range(len(s)):
             if s[i] == '(':
                 stack.append(i)
