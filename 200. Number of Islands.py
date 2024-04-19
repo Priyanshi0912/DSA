8bb04ce3bc6bf16834f0ec31c752f35292835fa9
Priyanshi <101731203+Priyanshi0912@users.noreply.github.com>
2024-04-19 23:37:16 +0530

    Create 200. Number of Islands.py
    Approach used: DFS( Depth first search)

---
+++ 200. Number of Islands.py
@@ -0,0 +1,23 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        n=len(grid)
+        m=len(grid[0])
+        ans=0
+        visited=set()
+        def dfs(grid,i:int,j:int):
+            if (i<0 or j<0 or i==n or j==m or grid[i][j]=="0" ):
+                return
+            grid[i][j]="0"
+            dfs(grid,i,j+1)
+            dfs(grid,i+1,j)
+            dfs(grid,i-1,j)
+            dfs(grid,i,j-1)
+        
+        for row in range(n):
+            for col in range(m):
+                if grid[row][col]=="1":
+                    
+                    ans+=1
+                    dfs(grid,row,col)
+
+        return ans
