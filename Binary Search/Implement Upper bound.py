228aed28290e3a77f1e9c0ca848b028458a5a8a2
Priyanshi <101731203+Priyanshi0912@users.noreply.github.com>
2024-10-22 21:26:26 +0530

    Create Implement Upper bound.py

---
+++ Binary Search/Implement Upper bound.py
@@ -0,0 +1,14 @@
+def upperBound(arr: [int], x: int, n: int) -> int:
+    # Write your code here.
+    ans=n
+    low=0
+    high=n-1
+    while(low<=high):
+        mid=(low+high)//2
+        if arr[mid]>x:
+            ans=mid
+            high=mid-1
+        else:
+            low=mid+1
+    return ans
+    pass
